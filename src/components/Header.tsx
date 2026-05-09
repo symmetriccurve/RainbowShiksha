ce18b7e6f4180852c38536f31ae169809d63b210
Vikram Belde <symmetriccurve@gmail.com>
2026-05-09 23:52:46 +0530

    Bundle update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,14 +21,14 @@ const Header: React.FC = () => {
   return (
     <header className="bg-white shadow-sm sticky top-0 z-50">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-32">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
-            <img src={logo} alt="Rainbow Shiksha" className="h-10 w-auto" />
+            <img src={logo} alt="Rainbow Shiksha" className="h-20 w-auto" />
           </Link>
 
           {/* Desktop Navigation */}
-          <div className="hidden md:flex items-center space-x-8">
+          <div className="hidden md:flex flex-1 items-center justify-around ml-8">
             {navigation.map((item) => (
               <Link
                 key={item.name}
